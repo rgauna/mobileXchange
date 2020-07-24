6094f12db34bb903cd3841f66f69a67d1eee9f16
hozerpffp <hozerpffp2009@gmail.com>
2020-07-23 21:05:18 -0500

    added gitignore file, removed config.json and replaced with connection.js. added js code into orm.js, phonecontroller.js, phone.js, server.js also added into seeds folder

---
+++ config/connection.js
@@ -0,0 +1,25 @@
+var mysql = require("mysql");
+var http = require("http");
+var connection;
+// Setting up mysql connection
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: "Rootbeer1",
+        database: 'mobileXchange'
+    });
+};
+// Make connection.
+connection.connect((err) => {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
+
+// Export connection for our ORM to use.
+module.exports = connection;
