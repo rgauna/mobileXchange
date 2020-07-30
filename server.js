660756ca8ca0d9225021116d69a4b878a9aca0c1
rudy gauna <rudy.gauna@gmail.com>
2020-07-29 23:47:45 -0500

    made changes to get authentication working

--- server.js
+++ server.js
@@ -1,12 +1,98 @@
 const express = require("express");
 const PORT = process.env.PORT || 8080;
+const bcrypt = require('bcrypt')
+const passport = require('passport')
+const flash = require('express-flash')
+const session = require('express-session')
+const methodOverride = require('method-override')
 const app = express();
 var db = require("./models");
+
+// const initializePassport = require('./config/passport')
+// initializePassport(
+//   passport,
+//   email => users.find(user => user.email === email),
+//   id => users.find(user => user.id === id)
+// )
+app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+
+
+const users = []
+
+app.set('view-engine', 'express-handlebars')
+app.use(express.urlencoded({ extended: false }))
+app.use(flash())
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(methodOverride('_method'))
+
+app.get('/', checkAuthenticated, (req, res) => {
+  res.render('main.handlebars', { name: req.user.name })
+})
+
+app.get('/login', checkNotAuthenticated, (req, res) => {
+  res.render('login.handlebars')
+})
+
+app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/login',
+  failureFlash: true
+}))
+
+app.get('/signup', checkNotAuthenticated, (req, res) => {
+  res.render('signup.handlebars')
+})
+
+app.post('/signup', checkNotAuthenticated, async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10)
+    users.push({
+      id: Date.now().toString(),
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword
+    })
+    res.redirect('/login')
+  } catch {
+    res.redirect('/signup')
+  }
+})
+
+app.delete('/logout', (req, res) => {
+  req.logOut()
+  res.redirect('/login')
+})
+
+function checkAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next()
+  }
+
+  res.redirect('/login')
+}
+
+function checkNotAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return res.redirect('/')
+  }
+  next()
+}
+
+app.listen(3000)
+
+
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 // Parse application body as JSON
 app.use(express.urlencoded({
-    extended: true
+    extended: true  
 }));
 app.use(express.json());
 // Set Handlebars.
@@ -19,7 +105,7 @@ app.set("view engine", "handlebars");
 require("./routes/buy-api-routes.js")(app);
 require("./routes/html-routes")(app);
 require("./routes/sell-api-routes")(app);
-require("./routes/HTMLroutes")(app);
+require("./routes/login-HTMLroutes")(app);
 require("./routes/login-api-routes")(app);
 // Start our server so that it can begin listening to client requests.
 db.sequelize.sync({
